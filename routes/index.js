0b116c899a1fd5949193167bb5cf5f484629285b
Matt Farmer <matt@frmr.me>
2013-06-23 22:10:31 -0400

    Add emails notifications.

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,9 @@ exports.build = function(app) {
   var mongodb = require('mongodb'),
       MongoClient = mongodb.MongoClient,
       mongoUrl = process.env.MONGOHQ_URL || "mongodb://127.0.0.1:27017/lmnominations-dev",
-      Mandrill = require('mandrill-api').Mandrill;
+      Mandrill = require('mandrill-api').Mandrill,
+      mandrillApi = new Mandrill(),
+      nominationInfoTargetEmail = process.env.NOMINATION_ALERT_EMAIL || "matt+nominfo@frmr.me";
 
   /**
    * Homepage.
@@ -11,7 +13,54 @@ exports.build = function(app) {
     res.render("index", {
       title: "Leadership Macon 2014 Nomination Form"
     });
-  };
+  }
+
+  function sendNominationInfoEmail(nomination) {
+    var data = {
+      nominationFields: nomination
+    };
+
+    app.render("emails/nomination-info", data, function(err, html) {
+      if (err) throw err;
+
+      mandrillApi.messages.send({
+        message: {
+          html: html,
+          subject: "New Nomination for LM 2014",
+          to: [
+            {
+              email: nominationInfoTargetEmail,
+              name: "Leadership Macon"
+            }
+          ]
+        }
+      });
+    });
+  }
+
+  function sendConfirmationEmail(to, nominatorName, nomineeName) {
+    var data = {
+      nominatorName: nominatorName,
+      nomineeName: nomineeName
+    };
+
+    app.render("emails/confirmation", data, function(err, html) {
+      if (err) throw err;
+
+      mandrillApi.messages.send({
+        message: {
+          html: html,
+          subject: "Thank you for your Nomination!",
+          to: [
+            {
+              email: to,
+              name: nominatorName
+            }
+          ]
+        }
+      });
+    });
+  }
 
   /**
    * Take in a nomination submission and store it in the
@@ -25,6 +74,13 @@ exports.build = function(app) {
         db.close();
 
         if (err) throw err;
+
+        sendNominationInfoEmail(req.body);
+        sendConfirmationEmail(
+          req.body["nominator-email"],
+          req.body["nominator-first-name"] + " " + req.body["nominator-last-name"],
+          req.body["nominee-first-name"] + " " + req.body["nominee-last-name"]
+        );
 
         res.render("thanks", {
           title: "Thank you!"
