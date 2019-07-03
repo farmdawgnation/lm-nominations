438f71fa65b7f919f36aa932820f1554b674f3eb
Matt Farmer <matt@frmr.me>
2019-07-02 22:18:52 -0400

    Replace 2019 with 2020

--- routes/index.js
+++ routes/index.js
@@ -14,7 +14,7 @@ exports.build = function(app) {
   **/
   function index(req, res){
     res.render("index", {
-      title: "Leadership Macon 2019 Nomination Form"
+      title: "Leadership Macon 2020 Nomination Form"
     });
   }
 
@@ -35,7 +35,7 @@ exports.build = function(app) {
       let mailOptions = {
         from: 'Leadership Macon <noreply@leadershipmacon.org>',
         to: 'Leadership Macon <' + nominationInfoTargetEmail + '>',
-        subject: 'New Nomination for LM 2019',
+        subject: 'New Nomination for LM 2020',
         html: renderedEmail
       };
 
